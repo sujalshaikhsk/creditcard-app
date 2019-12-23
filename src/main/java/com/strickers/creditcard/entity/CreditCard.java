be40253ae926215ad86a3d2fb461402681eaf09d
User1 <sujalshaikhsk@gmail.com>
2019-12-23 12:58:02 +0530

    customer entity added

--- src/main/java/com/strickers/creditcard/entity/CreditCard.java
+++ src/main/java/com/strickers/creditcard/entity/CreditCard.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -26,12 +28,10 @@ public class CreditCard implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "creditcardsequence")
 	private Long creditCardNumber;
 
-	private String customerName;
-	private Long mobileNumber;
-	private String address;
-	private String gender;
-	private LocalDate dateOfBirth;
-	private Double salary;
+	@OneToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
 	private Double availableBalance;
 	private Double creditCardLimit;
 	private LocalDate validFrom;
